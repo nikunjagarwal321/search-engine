7a220a1fd296b2d636e592ef7788c33b7fb99cfe
Nikunj Agarwal <nikunjagarwal321@gmail.com>
2021-11-14 15:35:04 +0530

    Initialized Index Service

---
+++ indexservice/src/main/java/com/searchengine/indexservice/IndexserviceApplication.java
@@ -0,0 +1,13 @@
+package com.searchengine.indexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexserviceApplication.class, args);
+	}
+
+}
---
+++ indexservice/src/test/java/com/searchengine/indexservice/IndexserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchengine.indexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
